13689715e776368d40c5370953338bbe3c2300d9
cutright <dan.cutright@gmail.com>
2020-11-23 20:06:19 -0600

    LGTM alert fix

--- IQDMPDF/pdf_reader.py
+++ IQDMPDF/pdf_reader.py
@@ -16,7 +16,6 @@
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfpage import PDFTextExtractionNotAllowed
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.converter import TextConverter
